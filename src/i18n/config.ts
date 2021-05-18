7b3a8fb1cca3218612f006b695b67e0390fc5b1f
sebastian.cieslik <sebastian.cieslik@skyrise.tech>
2021-05-18 13:46:53 +0200

    App setup - chakra, i18next

---
+++ src/i18n/config.ts
@@ -0,0 +1,32 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Common from "./en/common.json";
+
+export const resources = {
+  en: {
+    Common,
+  },
+} as const;
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: "en",
+    fallbackLng: "en",
+    ns: ["Common"],
+    defaultNS: "Common",
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: false,
+    },
+    resources,
+  });
+
+export default i18n;
