4a4b33149a0f83adadde7b4698af72bd2a9cf942
sebastian.cieslik <sebastian.cieslik@skyrise.tech>
2021-06-16 14:43:36 +0200

    Set document title using react-helmet

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,10 +1,16 @@
 import React, { FC } from 'react';
 import { Flex, Box, Text } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+import { Helmet } from 'react-helmet';
 import { CreateNewInvoice, InvoiceList, InvoiceStatusFilter } from 'components';
 
 export const DashboardView: FC = () => {
+  const { t } = useTranslation('InvoiceList');
   return (
     <Flex w="100%" maxW="730px" h="100%" flexDirection="column" ml={[2, 8, '120px']} mr={[2, 8, 6, '120px']}>
+      <Helmet>
+        <title>{t('TITLE')}</title>
+      </Helmet>
       <Flex justifyContent="space-between" alignItems="center" px={[4, 0]}>
         <Text as="h2" textStyle="h1">
           Invoices
--- src/views/SignUpView.tsx
+++ src/views/SignUpView.tsx
@@ -1,10 +1,16 @@
 import React, { FC } from 'react';
 import { Flex } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+import { Helmet } from 'react-helmet';
 import { SignUp, ColorModeSwitcher, Logo } from 'components';
 
 export const SignUpView: FC = () => {
+  const { t } = useTranslation('SignUp');
   return (
     <Flex w="100%" h="100%" justifyContent="center" alignItems="center">
+      <Helmet>
+        <title>{t('TITLE')}</title>
+      </Helmet>
       <Flex position="absolute" top="0" left="0" w="100%" p={5} justifyContent="space-between">
         <Logo />
         <ColorModeSwitcher />
